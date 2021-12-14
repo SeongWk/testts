2ead63507da527e34b8100e7e074692eb2e388c2
Seong Wook Lim <40172001+SeongWk@users.noreply.github.com>
2021-12-14 15:04:43 +0900

    Add files via upload

---
+++ ActopmSegue/ActopmSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActopmSegue
+//
+//  Created by seong_wook_lim on 2021/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
